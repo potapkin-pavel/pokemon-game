94695664edfbf73f5d79d10e833435eefe8db6a2
Pavel Potapkin <pavelpotapkin@gmail.com>
2021-01-30 02:08:35 +0100

    Update text content of first and second layouts.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -141,11 +141,23 @@ const pokemons = [
 function App () {
   return (
     <>
-      <Header title='Title' desc='Description'/>
-      <Layout id='1' title='Title' desc='Description' urlBg={bg} />
-      <Layout id='2' title='Title' desc='Description' colorBg='#d4d4d4' />
-      <Layout id='3' title='Title' desc='Description' urlBg={bg} />
-      <Footer />
+      <Header title='This is First Layout' desc='Description'/>
+      <Layout id='1' title='Title' urlBg={bg}>
+        <p>In the game two players face off against one another, one side playing as &quot;blue&quot;,
+          the other as &quot;red&quot; on a 3x3 grid.</p>
+        <p>Each player has five cards in a hand and the aim is to capture the opponent&apos;s cards by
+          turning them into the player&apos;s own color of red or blue.</p>
+      </Layout>
+      <Layout id='3' title='This is Third Layout' urlBg={bg}>
+        <p>To win, a majority of the total ten cards played (including the one card that is not placed
+          on the board) must be of the player&apos;s card color. To do this, the player must capture cards
+          by placing a card adjacent to an opponent&apos;s card whereupon the &lsquo;ranks&lsquo; of the
+          sides where the two cards touch will be compared. If the rank of the opponent&apos;s card is
+          higher than the player&apos;s card, the player&apos;s card will be captured and turned into the
+          opponent&apos;s color. If the player&apos;s rank is higher, the opponent&apos;s card will be
+          captured and changed into the player&apos;s color instead.</p>
+      </Layout>
+      <Footer/>
     </>
   )
 }
