4ead4de3a3d2b61770499b7c61440795319b580d
Pavel Potapkin <pavelpotapkin@gmail.com>
2021-01-30 02:10:13 +0100

    Add pokemon card component and pass pokemons into.

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,6 +1,7 @@
+import Footer from '../footer'
 import Header from '../header'
 import Layout from '../layout'
-import Footer from '../footer'
+import PokemonCard from '../pokemon-card'
 import bg from '../../assets/bg3.jpg'
 const pokemons = [
   {
@@ -147,6 +148,12 @@ function App () {
           the other as &quot;red&quot; on a 3x3 grid.</p>
         <p>Each player has five cards in a hand and the aim is to capture the opponent&apos;s cards by
           turning them into the player&apos;s own color of red or blue.</p>
+      </Layout>
+      <Layout id='2' title='This is Second Layout' colorBg='#d4d4d4'>
+        <div className={s.flex}>
+          {pokemons.map(pokemon => <PokemonCard key={pokemon.id} name={pokemon.name} img={pokemon.img}
+          id={pokemon.id} type={pokemon.type} values={pokemon.values}/>)}
+        </div>
       </Layout>
       <Layout id='3' title='This is Third Layout' urlBg={bg}>
         <p>To win, a majority of the total ten cards played (including the one card that is not placed
