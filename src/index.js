59c4781efe9cb909a6c75de31c89d8898e4a0b59
Pavel Potapkin <pavelpotapkin@gmail.com>
2021-02-04 18:10:18 +0100

    Wrap App component in Browser Router.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/app/'
+import { BrowserRouter } from 'react-router-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+ReactDOM.render(<BrowserRouter><App/></BrowserRouter>, document.getElementById('root'))
