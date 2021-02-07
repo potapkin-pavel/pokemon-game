04199816bce083d39555ce88ee2116de24512504
Pavel Potapkin <pavelpotapkin@gmail.com>
2021-02-07 17:35:24 +0100

    Add getting of pokemon cards from firebase and updating pokemon active state.

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -1,28 +1,43 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import Footer from '../../components/footer'
 import Layout from '../../components/layout'
 import PokemonCard from '../../components/pokemon-card'
 
+import database from '../../services/firebase'
+
 import s from './style.module.css'
 
-import pokemonsArray from '../../data/pokemons.json'
-
 function GamePage () {
-  const [pokemons, setPokemons] = useState(pokemonsArray)
+  const [pokemons, setPokemons] = useState({})
 
-  const handleCardClick = (text) => {
-    setPokemons(pokemonsArray => pokemonsArray.map(item =>
-      item.id === text ? { ...item, isActive: !item.isActive } : item))
+  const handleCardClick = (key, id) => {
+    setPokemons(prevState => {
+      return Object.entries(prevState).reduce((acc, item) => {
+        const pokemon = { ...item[1] }
+        if (pokemon.id === id) {
+          pokemon.active = !(pokemon.active)
+          database.ref('pokemons/' + key).set(pokemon)
+        }
+        acc[item[0]] = pokemon
+        return acc
+      }, {})
+    })
   }
+
+  useEffect(() => {
+    database.ref('pokemons').on('value', (snapshot) => {
+      setPokemons(snapshot.val())
+    })
+  }, [])
 
   return (
     <>
       <Layout id='1' title='This is Game Page' colorBg='#d4d4d4'>
         <div className={s.flex}>
-          {pokemons.map(pokemon =>
-            <PokemonCard key={pokemon.id} name={pokemon.name} img={pokemon.img} id={pokemon.id} type={pokemon.type}
-              values={pokemon.values} isActive={pokemon.isActive} onCardClick={() => handleCardClick(pokemon.id)}/>)}
+          {Object.entries(pokemons).map(([key, { name, img, id, type, values, active }]) =>
+            <PokemonCard key={key} name={name} img={img} id={id} type={type} values={values} isActive={active}
+              onCardClick={() => handleCardClick(key, id)}/>)}
         </div>
       </Layout>
       <Footer/>
