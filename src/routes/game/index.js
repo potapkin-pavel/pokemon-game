c123fcd16fe2b0ba838609489f63a8dd478b2ba7
Pavel Potapkin <pavelpotapkin@gmail.com>
2021-02-14 18:00:37 +0100

    Rearrange components and routes.

--- src/routes/game/index.js
+++ src/routes/game/index.js
@@ -1,22 +1,46 @@
 import { useState } from 'react'
 import { Route, useRouteMatch, Switch } from 'react-router-dom'
 
-import StartPage from './routes/start'
-import BoardPage from './routes/board'
-import FinishPage from './routes/finish'
+import StartPage from '../start'
+import BoardPage from '../board'
+import FinishPage from '../finish'
 
 import { PokemonContext } from '../../context/pokemon-context'
 
 function GamePage () {
   const [pokemons, setPokemons] = useState([])
+  const [player1Cards, setPlayer1Cards] = useState([])
+  const [player2Cards, setPlayer2Cards] = useState([])
+  const [gameResult, setGameResult] = useState('')
 
   const handlerPokemonsChange = (pokemons) => {
     setPokemons(pokemons)
   }
+  
+  const handlePlayer1Cards = (pokemons) => {
+    setPlayer1Cards(pokemons)
+  }
+  
+  const handlePlayer2Cards = (pokemons) => {
+    setPlayer2Cards(pokemons)
+  }
+
+  const handleGameResult = (gameResult) => {
+    setGameResult(gameResult)
+  }
 
   const match = useRouteMatch()
   return (
-    <PokemonContext.Provider value={{ pokemons, onChangePokemon: handlerPokemonsChange }}>
+    <PokemonContext.Provider value={{
+      pokemons,
+      gameResult,
+      onGameResult: handleGameResult,
+      onChangePokemon: handlerPokemonsChange,
+      onChangePlayer1Cards: handlePlayer1Cards,
+      onChangePlayer2Cards: handlePlayer2Cards,
+      player1Cards,
+      player2Cards
+    }}>
       <Switch>
         <Route path={`${match.path}/`} exact component={StartPage} />
         <Route path={`${match.path}/board`} component={BoardPage} />
--- src/routes/game/routes/finish/index.js
+++
@@ -1,7 +0,0 @@
-const FinishPage = () => {
-  return (
-    <h1>Finish Page</h1>
-  )
-}
-
-export default FinishPage
